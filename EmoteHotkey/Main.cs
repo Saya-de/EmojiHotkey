2b1a61a4cc9f9354a87113d0e2eaf281feb390c1
Saya-de <sven_behrend@web.de>
2022-01-22 19:12:06 +0100

    Projektdateien hinzufügen.

---
+++ EmoteHotkey/Main.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEngine;
+using VRC.SDKBase;
+
+
+namespace EmojiHotkey
+{
+    public static class Main
+    {
+        public const string Name = "EmojiHotkey";
+        public const string Description = "Hotkey=Emoji";
+        public const string Author = "Saya";
+        public const string Company = null;
+        public const string Version = "1.8.7";
+        public const string DownloadLink = null;
+    }
+
+    public class EmojiHotkey : MelonMod
+    {
+        public override void OnUpdate()
+        {            
+             if (Input.GetMouseButtonDown(2))
+             {            
+                SendEmoji(55);
+             }
+                       
+        }
+
+
+        private static void SendEmoji(int Number)
+        {
+            MelonLogger.Msg(ConsoleColor.Green, $"Sending Emoji 55");
+            Networking.RPC(RPC.Destination.All,
+                LocalVRCPlayer.gameObject,
+                "SpawnEmojiRPC",
+                new Il2CppSystem.Object[1] {
+                    new Il2CppSystem.Int32 {
+                        m_value = Number
+                    }.BoxIl2CppObject()
+                });
+        }
+
+        public static VRCPlayer LocalVRCPlayer
+        {
+            get
+            {
+                return VRCPlayer.field_Internal_Static_VRCPlayer_0;
+            }
+        }
+    }
+
+
+}
---
+++ EmoteHotkey/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using MelonLoader;
+
+[assembly: AssemblyTitle(EmojiHotkey.Main.Name)]
+[assembly: AssemblyDescription(EmojiHotkey.Main.Description)]
+[assembly: AssemblyCompany(EmojiHotkey.Main.Company)]
+[assembly: AssemblyProduct(EmojiHotkey.Main.Name)]
+[assembly: AssemblyCopyright("Created by " + EmojiHotkey.Main.Author)]
+[assembly: AssemblyTrademark(EmojiHotkey.Main.Company)]
+[assembly: AssemblyVersion(EmojiHotkey.Main.Version)]
+[assembly: AssemblyFileVersion(EmojiHotkey.Main.Version)]
+[assembly: MelonInfo(typeof(EmojiHotkey.EmojiHotkey), EmojiHotkey.Main.Name, EmojiHotkey.Main.Version, EmojiHotkey.Main.Author, EmojiHotkey.Main.DownloadLink)]
+[assembly: MelonColor()]
+
+// Create and Setup a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
+// If no MelonGame Attribute is found or any of the Values for any MelonGame Attribute on the Melon is null or empty it will be assumed the Melon is Universal.
+// Values for MelonGame Attribute can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame(null, null)]
